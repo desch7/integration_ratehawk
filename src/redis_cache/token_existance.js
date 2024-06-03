1c90319114fa17a2361c8ffadb606669638ca0e8
desch7 <deschanel.tchatchi@gmail.com>
2024-06-03 14:53:12 +0100

    redis cache implement

---
+++ src/redis_cache/token_existance.js
@@ -0,0 +1,57 @@
+const redis = require("redis");
+
+// verify : if token already exist in redis cache else it will be insert in redis cache and will be delete after 30 days
+const tokenExistance = async (newToken) => {
+    let cachedToken = ''
+    let result = ''
+    const redisClient = redis.createClient({
+        socket: {
+            reconnectStrategy: function (retries = 10) {
+                if (retries > 20) {
+                    console.log("Too many attempts to reconnect. Redis connection was terminated");
+                    return new Error("Too many retries.");
+                } else {
+                    return retries * 500;
+                }
+            }
+        },
+        //url: redis[s]://[[username][:password]@][host][:port][/db-number] exemple: 'redis://alice:foobared@awesome.redis.server:6380'
+    })
+
+
+    // insertion of token and setting of expiration time
+    const insertToken = async (key, token, days) => {
+        const expirationTimeInSeconds = days * 24 * 60 * 60;
+
+        await redisClient.set(key, token, { EX: expirationTimeInSeconds })
+    };
+
+    // scan if the token already exist
+    const scanAllTokens = async () => {
+        for await (const ke of redisClient.scanIterator()) {
+            let tokenInLoop = await redisClient.get(ke);
+            cachedToken = tokenInLoop === newToken ? tokenInLoop : ''
+            if (cachedToken) {
+                return 'ok'
+            }
+        }
+    }
+
+    await redisClient.connect()
+        .then(async (res) => {
+            let tokenExist = await scanAllTokens()
+            if (tokenExist !== 'ok') {
+                await insertToken(String(Date.now()), newToken, process.env.REDIS_TOKEN_DAYS_EXPIRATION)
+                redisClient.quit();
+                result = 'ok'
+            } else {
+                redisClient.quit();
+                result = 'ko'
+            }
+        })
+        .catch((err) => { console.error('Redis error: ', err); })
+
+    return result
+}
+
+module.exports = tokenExistance;
